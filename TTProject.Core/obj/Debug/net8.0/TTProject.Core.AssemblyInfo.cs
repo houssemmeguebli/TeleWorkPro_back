086d75ce31144808b64aa8cf635486ce9d0478c6
houssemmeguebli <megbli.houssam@gmail.com>
2024-09-03 15:14:35 +0100

    Before merge

--- TTProject.Core/obj/Debug/net8.0/TTProject.Core.AssemblyInfo.cs
+++ TTProject.Core/obj/Debug/net8.0/TTProject.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TTProject.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+752b3d7ea9ca0e8960a1703f230b8150afa21484")]
 [assembly: System.Reflection.AssemblyProductAttribute("TTProject.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TTProject.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
